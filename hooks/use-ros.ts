c06e3ef360a0e26bb2569bebdb86c35eb016bc89
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-10-08 14:40:57 +0700

    read navigating goal status

--- hooks/use-ros.ts
+++ hooks/use-ros.ts
@@ -10,19 +10,34 @@ export interface IJoyCommand {
   yaw: number;
 };
 
+export interface NavFeedback {
+  nav_time_sec: number;
+  ETA: number;
+  distance_remaining_meter: number;
+}
+
+type NavStatusType = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 export default function useROS() {
   const rosRef = useRef<ROSLIB.Ros | null>(null);
   const cmdVelRef = useRef<ROSLIB.Topic | null>(null);
   const chatServiceRef = useRef<ROSLIB.Service | null>(null);
   const getWaypointsServiceRef = useRef<ROSLIB.Service | null>(null);
+  const navActionFeedbackSubRef = useRef<ROSLIB.Topic | null>(null);
+  const navActionStatusSubRef = useRef<ROSLIB.Topic | null>(null);
+  const [navFeedback, setNavFeedback] = useState<NavFeedback | null>(null);
+  const [navStatus, setNavStatus] = useState<NavStatusType>(0)
   const [connected, setConnected] = useState<boolean>(false);
 
   const disconnect = () => {
     if (rosRef.current != null) {
-      rosRef.current.close();
       cmdVelRef.current = null;
       chatServiceRef.current = null;
       getWaypointsServiceRef.current = null;
+      navActionStatusSubRef.current?.unsubscribe()
+      navActionStatusSubRef.current = null;
+      navActionFeedbackSubRef.current?.unsubscribe();
+      navActionFeedbackSubRef.current = null;
+      rosRef.current.close();
       rosRef.current = null;
     }
     if (connected) {
@@ -59,6 +74,38 @@ export default function useROS() {
       messageType: 'sensor_msgs/Joy',
     });
 
+    const navActionFeedbackTopic = new ROSLIB.Topic({
+      ros: ros,
+      name: '/navigate_to_pose/_action/feedback',
+      messageType: 'nav2_msgs/action/NavigateToPose_FeedbackMessage',
+    })
+
+    navActionFeedbackTopic.subscribe((msg: any) => {
+      console.log('Received feedback:', msg)
+
+      setNavFeedback({
+        nav_time_sec: msg.navigation_time.sec,
+        distance_remaining_meter: msg.distance_remaining,
+        ETA: msg.estimated_time_remaining.sec,
+      })
+    })
+
+    const navActionStatusTopic = new ROSLIB.Topic({
+      ros: ros,
+      name: '/navigate_to_pose/_action/status',
+      messageType: 'action_msgs/msg/GoalStatusArray',
+    })
+
+    navActionStatusTopic.subscribe((msg: any) => {
+      console.log('Received status:', msg)
+
+      if (msg.status_list.length === 0) {
+        setNavStatus(0)
+        return
+      }
+      setNavStatus(msg.status_list[msg.status_list.length - 1].status)
+    })
+
     const chatService = new ROSLIB.Service({
       ros: ros,
       name: '/prompt',
@@ -76,6 +123,8 @@ export default function useROS() {
     cmdVelRef.current = cmdVel
     chatServiceRef.current = chatService
     getWaypointsServiceRef.current = getWaypointsService;
+    navActionFeedbackSubRef.current = navActionFeedbackTopic
+    navActionStatusSubRef.current = navActionStatusTopic
 
     setConnected(true);
   }
@@ -93,7 +142,17 @@ export default function useROS() {
     cmdVelRef.current.publish(joy);
   };
 
-  return { ros: rosRef.current, sendVelocity, connected, connect, disconnect, getWaypoints: getWaypointsServiceRef.current, chatService: chatServiceRef.current };
+  return {
+    ros: rosRef.current,
+    sendVelocity,
+    connected,
+    connect,
+    disconnect,
+    getWaypoints: getWaypointsServiceRef.current,
+    chatService: chatServiceRef.current,
+    navStatus,
+    navFeedback,
+  };
 
 }
 
